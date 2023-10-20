10612f505bab74cf6c9eeb529bd0a643f2620d37
ShinichiUno <shin118h16@gmail.com>
2023-10-20 16:03:02 +0900

    final-spike-basic_motionに注釈を追加

--- src/final/spike/basic_motion_testUNO.py
+++ src/final/spike/basic_motion_testUNO.py
@@ -1,35 +1,34 @@
 # 2023 obstacle
 import hub
 class Basic_motion:
-    def __init__(self,motor_steer,motor):
+    def __init__(self,motor_steer,motor): # 使用するモーターを定義
         self.motor_steer = motor_steer
         self.motor = motor
 
 
-    def move(self,throttle, steer):
+    def move(self,throttle, steer): # 一定のステアリング値と一定のスピードで走行する関数
         self.motor.run_at_speed(throttle)
         once = False
         count = 0
 
-        if steer > 90:
+        if steer > 90:   # 無理な角度にならないように一定以上の値はカットする
             steer = 90
         elif steer < -90:
             steer = -90
 
-        elif steer > 70:
+        elif steer > 70: # 同様に一定以上の値はカットする
             steer = 70
         elif steer < -70:
             steer = -70
 
         while True:
-
-            if(self.motor_steer.busy(type=1)): #if motor_steer is moving
+            if(self.motor_steer.busy(type=1)): # if motor_steer is moving 
                 count = count + 1
                 continue
-            elif once:
+            elif once: # 一度だけ動かすため
                 break
             else:
-                self.motor_steer.run_to_position(steer)
+                self.motor_steer.run_to_position(steer) # ステアリングモータの角度を[steer]にする
                 once = True
 
 
